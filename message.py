b9d39dabacfe48eff4c7c81ceb849bcee6f2ee96
Aaron Thompson <acthompson.w@gmail.com>
2016-03-07 04:00:31 -0800

    Initial commit
    single-user, single-session, no edit capability, uses print('\n' * 50) to "clear" the screen

---
+++ message.py
@@ -0,0 +1,64 @@
+def cls(): print('\n' * 50)
+message_01 = ''
+message_02 = ''
+message_03 = ''
+message_04 = ''
+message_05 = ''
+
+while message_01 == '':
+    cls()
+    print('01. ' + message_01)
+    print('02. ' + message_02)
+    print('03. ' + message_03)
+    print('04. ' + message_04)
+    print('05. ' + message_05)
+    print('>> Leave a message!')
+    message_01 = input()
+
+while message_02 == '':
+    cls()
+    print('01. ' + message_01)
+    print('02. ' + message_02)
+    print('03. ' + message_03)
+    print('04. ' + message_04)
+    print('05. ' + message_05)
+    print('>> Leave a message!')
+    message_02 = input()
+
+while message_03 == '':
+    cls()
+    print('01. ' + message_01)
+    print('02. ' + message_02)
+    print('03. ' + message_03)
+    print('04. ' + message_04)
+    print('05. ' + message_05)
+    print('>> Leave a message!')
+    message_03 = input()
+
+while message_04 == '':
+    cls()
+    print('01. ' + message_01)
+    print('02. ' + message_02)
+    print('03. ' + message_03)
+    print('04. ' + message_04)
+    print('05. ' + message_05)
+    print('>> Leave a message!')
+    message_04 = input()
+
+while message_05 == '':
+    cls()
+    print('01. ' + message_01)
+    print('02. ' + message_02)
+    print('03. ' + message_03)
+    print('04. ' + message_04)
+    print('05. ' + message_05)
+    print('>> Leave a message!')
+    message_05 = input()
+
+cls()
+print('01. ' + message_01)
+print('02. ' + message_02)
+print('03. ' + message_03)
+print('04. ' + message_04)
+print('05. ' + message_05)
+print('>> Message box full.')
