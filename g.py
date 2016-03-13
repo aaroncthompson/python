c6c3dbcae21acec11798d361f2afa4f9d6753294
Aaron Thompson <acthompson.w@gmail.com>
2016-03-12 16:08:19 -0800

    Create g.py

---
+++ g.py
@@ -0,0 +1,20 @@
+allies = []
+enemies = []
+
+if 'dwarf' in allies:
+  # passive buff
+  # dwarf discount
+  # dwarf appearance in final battle
+  # negative effect on relationship with elf
+  
+if 'elf' in allies:
+  # passive buff
+  # elf discount
+  # elf appearance in final battle
+  # negative effect on relationship with dwarf
+  
+if 'dwarf' in enemies:
+  # positive effect on relationship with elf
+  
+if 'elf' in enemies:
+  # positive effect on relationship with dwarf
