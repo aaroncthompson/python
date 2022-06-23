8f83675b2253c83c5929be2995e80ee7eb85bcb0
root <root@ip-172-31-17-17.us-west-2.compute.internal>
2022-06-23 04:41:09 +0000

    Found that using while() did in fact seem to use less memory.

--- 1-messageboard.py
+++ 1-messageboard.py
@@ -6,16 +6,17 @@ from datetime import datetime,timezone
 messages = [{'name': 'Aaron', 'date':'2022-06-22 05:07:20.884325+00:00', 'message':'Hey there! Hope you\'re having a pretty okay day.'}]
 
 def welcome():
-    print(f"Welcome to the messageboard! It is currently {datetime.now(timezone.utc)}.\nThere are {len(messages)} messages.")
-    choice = input('Type "save" (without quotes) to save a message, "show" (without quotes) to list the messages saved so far, or "exit" to exit.\n>> ')
-    if choice == "save":
-        save_message()
-    elif choice == "show":
-        show_messages()
-    elif choice == "exit":
-        print("Exiting!")
-    else:
-        print("Invalid choice. Exiting!")
+    while input != "exit":
+        print(f"Welcome to the messageboard! It is currently {datetime.now(timezone.utc)}.\nThere are {len(messages)} messages.")
+        choice = input('Type "save" (without quotes) to save a message, "show" (without quotes) to list the messages saved so far, or "exit" to exit.\n>> ')
+        if choice == "save":
+            save_message()
+        elif choice == "show":
+            show_messages()
+        elif choice == "exit":
+            print("Exiting!")
+        else:
+            print("Invalid choice.")
 
 def save_message():
     entry = {}
@@ -25,16 +26,14 @@ def save_message():
     entry['date'] = str(datetime.now(timezone.utc))
     entry['message'] = input("What is your message? >> ")
     messages.append(entry)
-    welcome()
 
 def show_messages():
     for each_message in messages:
         print(f"{each_message['name']} said at {each_message['date']}: {each_message['message']}")
-    welcome()
 
 welcome()
 
-# is that, like.. okay? calling a function from another function - seems like it could potentially pick up some overhead as they nest? idk what i'm talking about
+
 
 # 3.0 should allow the user to selectively display messages from a certain date (let's just stick to month/day) and/or name
 # 4.0 should actually save the messages somewhere and have them persist
