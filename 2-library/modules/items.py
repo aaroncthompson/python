cf9619b53cebb57aeb4fa23a3e906707438d13cf
aaronct <aaronct@amazon.com>
2022-06-27 06:11:24 +0000

    Added (yet-unsed) item/book/video/audio classes

---
+++ 2-library/modules/items.py
@@ -0,0 +1,37 @@
+class Item():
+    """An item belonging to our library, which can be checked in and out."""
+
+    def __init__(self, title, author, year):
+        """Initialize title and author attributes."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def check_out(self):
+        """Check the item out."""
+        print(f"{self.title.title()} has been checked out. Enjoy!")
+
+    def check_in(self):
+        """Check the item in."""
+        print(f"{self.title.title()} has been checked in. Thanks!")
+
+class Book(Item):
+    """Text media belonging to our library."""
+
+    def __init__(self, title, author, year):
+        """Initialize attributes."""
+        super().__init__(title, author, year)
+
+class Video(Item):
+    """Video media belonging to our library."""
+
+    def __init__(self, title, author, year):
+        """Initialize attributes."""
+        super().__init__(title, author, year)
+
+class Audio(Item):
+    """Audio media belonging to our library."""
+
+    def __init__(self, title, author, year):
+        """Initialize attributes."""
+        super().__init__(title, author, year)
