87c6f8058348170688b1c4a972a82bb005f1753e
aaronct <aaronct@amazon.com>
2022-07-01 04:38:11 +0000

    It works now! Removed messageboard-legacy.py.

--- 1-messageboard/messageboard.py
+++ 1-messageboard/messageboard.py
@@ -4,15 +4,18 @@
 # import datetime modules so i can timestamp messages
 from datetime import datetime,timezone
 
-# import sys so i can call my modules
-import sys
+# import path so i can call my modules
+from sys import path
 # import my modules
-sys.path.append('modules')
-import modules/file_io as f_io
+path.append('modules')
+import file_io as fio
+import message_bank as mb
+
+messages = []
 
 def welcome():
     while True:
-        print(f"Welcome to the messageboard! It is currently {datetime.now(timezone.utc)}.\nThere are {len(messages)} messages.")
+        print(f"Welcome to the messageboard! It is currently {datetime.now(timezone.utc)}.\nThere are {len(mb.messages)} messages.")
         choice = input('Type "save" (without quotes) to save a message, "show" (without quotes) to list the messages saved so far, or "exit" to exit.\n>> ')
         if choice == "save":
             save_message()
@@ -34,18 +37,21 @@ def save_message():
         print(f"Bob isn't supposed to be using this.\nThis incident will be reported.")
     entry['date'] = str(datetime.now(timezone.utc))
     entry['message'] = input("What is your message? >> ")
-    messages.append(entry)
+    mb.messages.append(entry)
+    fio.write_file(mb.messages)
 
 def show_messages():
-    for each_message in messages:
+    fio.load_file()
+    for each_message in mb.messages:
         if "bob" in each_message['name'].lower() or "bob" in each_message['message'].lower():
             print("A message mentioning Bob has been removed.")
             continue
         print(f"{each_message['name']} said at {each_message['date']}: {each_message['message']}")
 
 # actual operation
-f_io.load_file()
+fio.load_file()
 welcome()
 
-# 3.0 should allow the user to selectively display messages from a certain date (let's just stick to month/day) and/or name
-# 4.0 should actually save the messages somewhere and have them persist
+# planned: don't have an entire module just for one variable
+# planned: refactoring
+# planned: allow user to selectively display messages based on certain criteria
--- 1-messageboard/modules/file_io.py
+++ 1-messageboard/modules/file_io.py
@@ -1,15 +1,16 @@
 import json
+import message_bank as mb
 
 def load_file():
     """Attempts to load messageboard with default name, otherwise starts us off with a default message."""
     try:
         with open('messageboard.json') as messageboard_file:
-            messages = messageboard_file.load(file_object)
+            mb.messages = json.load(messageboard_file)
     except FileNotFoundError:
-        messages = [{'name': 'Aaron', 'date':'2022-06-22 05:07:20.884325+00:00', 'message':'Hey there! Hope you\'re having a pretty okay day.'}]
+        mb.messages = [{'name': 'Aaron', 'date':'2022-06-22 05:07:20.884325+00:00', 'message':'Hey there! Hope you\'re having a pretty okay day.'}]
 
 # is there any benefit to appending new messages over just overwriting old+new? still have to load old to show them so it doesn't seem like we'd be saving memory.
-def write_file():
-    """Appends new messages to messageboard and clears new_messages."""
-    with open('messageboard.json', 'a') as messageboard_file:
-        json.dump(messages, messageboard_file)
+def write_file(updated_messages):
+    """Appends new messages to messageboard."""
+    with open('messageboard.json', 'w') as messageboard_file:
+        json.dump(updated_messages, messageboard_file)
